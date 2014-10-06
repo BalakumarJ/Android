47d09776833745f688f874ad1a3dd64ed8b79260
BalakumarJ <bala7777@email.tamu.edu>
2014-10-05 23:29:05 -0500

    MP5

---
+++ Multiactivity/src/com/andriod/multiactivity/DispActivity.java
@@ -0,0 +1,37 @@
+package com.andriod.multiactivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+
+public class DispActivity extends ActionBarActivity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disp);
+
+        
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+  
+}
